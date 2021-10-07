eba04e4b61381bd6cf154802b7c55a17613cb05c
marioarenasagudelo <43125622+marioarenasagudelo@users.noreply.github.com>
2021-10-06 22:37:06 -0500

    frontend-v1

---
+++ front-iservi/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <ServiciosDesatacados/>
+</template>
+
+<script>
+  import ServiciosDesatacados from '../components/Home/ServiciosDesatacados.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      ServiciosDesatacados,
+    },
+  }
+</script>
---
+++ front-iservi/src/views/Nosotros.vue
@@ -0,0 +1,15 @@
+<template>
+  <SobreNosotros/>
+</template>
+
+<script>
+  import SobreNosotros from '../components/Nosotros/SobreNosotros.vue'
+
+  export default {
+    name: 'Nosotros',
+
+    components: {
+      SobreNosotros,
+    },
+  }
+</script>
