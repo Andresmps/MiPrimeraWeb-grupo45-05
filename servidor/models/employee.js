d28f4ffdae11b96d9b30f3bba0a8cba2a7f50a40
marioarenasagudelo <43125622+marioarenasagudelo@users.noreply.github.com>
2021-10-06 22:30:25 -0500

    frontend-v1

---
+++ servidor/models/employee.js
@@ -0,0 +1,21 @@
+import mongoose, { SchemaTypes } from 'mongoose';
+const Schema = mongoose.Schema;
+
+const employees_schema = new Schema({
+    username: { type: String, required: [true, 'Username requeried'] },
+    password: { type: String, required: [true, 'Password requeried'] },
+    name: String,
+    email: String,
+    address: String,
+    phone_number: String,
+    created_at: { type: Date, default: Date.now },
+    employee_reliability: { type: Number, default: 0.7 },
+    employee_description: String,
+    experience: String,
+    services: [{
+        type: Schema.Types.ObjectId,
+        ref: 'services'
+    }],
+});
+
+module.exports = mongoose.model('employees', employees_schema);
